b2965e01bf2278a08140946fa6086ec19871b7be
nacong <nabinyang37@gmail.com>
2021-03-14 16:41:06 +0900

    second

--- public/point.js
+++ public/point.js
@@ -3,7 +3,7 @@ export class Point {
         this.x = x;
         this.y = y;
         this.fixedY = y;
-        this.speed = 0.02;
+        this.speed = 0.05;
         this.cur = index;
         this.max = Math.random() * (stageHeight / 20);      //바꾸려면 wave의 pointGapY도 바꿔야함
     }
